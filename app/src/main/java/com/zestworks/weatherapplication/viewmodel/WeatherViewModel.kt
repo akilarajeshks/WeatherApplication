05be4250e525c13c9439fd5a6fa7ead2749c59c5
akila-5574 <akila.r@zohocorp.com>
2019-07-15 00:56:11 +0530

    Location permission init Tests for location

--- app/src/main/java/com/zestworks/weatherapplication/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/zestworks/weatherapplication/viewmodel/WeatherViewModel.kt
@@ -3,36 +3,45 @@ package com.zestworks.weatherapplication.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.zestworks.weatherapplication.model.Forecast
-import com.zestworks.weatherapplication.model.Repository
+import com.zestworks.weatherapplication.repository.Repository
 
 class WeatherViewModel(private val repository: Repository) : ViewModel() {
 
     val currentStatus: MutableLiveData<Status> = MutableLiveData<Status>().apply { postValue(Status.None) }
 
+    enum class ErrorType {
+        NETWORK_FAILURE,
+        LOCATION_PERMISSION_DENIED
+    }
+
     sealed class Status {
         object None : Status()
         object Loading : Status()
         data class Success(val forecast: Forecast) : Status()
-        data class Error(val reason: String) : Status()
+        data class Error(val errorType: ErrorType) : Status()
     }
 
-    fun onFragmentStart(){
+    fun onLocationFetched(lat: Double, long: Double) {
         currentStatus.postValue(Status.Loading)
-        repository.loadWeatherData(object : NetworkCallback{
+        repository.loadWeatherData(lat, long, object : NetworkCallback {
             override fun onSuccess(forecast: Forecast) {
                 currentStatus.postValue(Status.Success(forecast))
             }
 
             override fun onFailure(reason: String) {
-                currentStatus.postValue(Status.Error(reason))
+                currentStatus.postValue(Status.Error(ErrorType.NETWORK_FAILURE))
             }
 
         })
     }
 
+    fun onLocationPermissionDenied(){
+        currentStatus.postValue(Status.Error(ErrorType.LOCATION_PERMISSION_DENIED))
+    }
+
 }
 
-interface NetworkCallback{
+interface NetworkCallback {
     fun onSuccess(forecast: Forecast)
     fun onFailure(reason: String)
 }
--- app/src/test/java/com/zestworks/weatherapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/zestworks/weatherapplication/ExampleUnitTest.kt
@@ -2,17 +2,14 @@ package com.zestworks.weatherapplication
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.zestworks.weatherapplication.model.Forecast
-import com.zestworks.weatherapplication.model.Repository
+import com.zestworks.weatherapplication.repository.Repository
 import com.zestworks.weatherapplication.viewmodel.NetworkCallback
 import com.zestworks.weatherapplication.viewmodel.WeatherViewModel
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentCaptor
-import org.mockito.Captor
-import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 
 /**
@@ -35,6 +32,9 @@ class ExampleUnitTest {
     @Captor
     lateinit var networkCallbackCaptor: ArgumentCaptor<NetworkCallback>
 
+    private val lat = 13.08
+    private val long = 80.28
+
     @Before
     fun setup() {
         weatherViewModel = WeatherViewModel(repository)
@@ -44,32 +44,42 @@ class ExampleUnitTest {
     fun `Test Failure`() {
         assert(weatherViewModel.currentStatus.value is WeatherViewModel.Status.None)
 
-        weatherViewModel.onFragmentStart()
+        weatherViewModel.onLocationFetched(lat,long)
 
         assert(weatherViewModel.currentStatus.value is WeatherViewModel.Status.Loading)
 
         val errorMessage = "Network failed"
-        Mockito.verify(repository).loadWeatherData(capture(networkCallbackCaptor))
+        Mockito.verify(repository).loadWeatherData(eq(lat),eq(long),capture(networkCallbackCaptor))
         networkCallbackCaptor.value.onFailure(errorMessage)
 
         assert(weatherViewModel.currentStatus.value is WeatherViewModel.Status.Error)
-        assert((weatherViewModel.currentStatus.value as WeatherViewModel.Status.Error).reason == errorMessage)
+        assert((weatherViewModel.currentStatus.value as WeatherViewModel.Status.Error).errorType == WeatherViewModel.ErrorType.NETWORK_FAILURE)
     }
 
     @Test
     fun `Test network success`() {
         assert(weatherViewModel.currentStatus.value is WeatherViewModel.Status.None)
 
-        weatherViewModel.onFragmentStart()
+        weatherViewModel.onLocationFetched(lat,long)
 
         assert(weatherViewModel.currentStatus.value is WeatherViewModel.Status.Loading)
 
         val forecast = Forecast()
-        Mockito.verify(repository).loadWeatherData(capture(networkCallbackCaptor))
+        Mockito.verify(repository).loadWeatherData(eq(lat), eq(long),capture(networkCallbackCaptor))
         networkCallbackCaptor.value.onSuccess(forecast = forecast)
 
         assert(weatherViewModel.currentStatus.value is WeatherViewModel.Status.Success)
         assert((weatherViewModel.currentStatus.value as WeatherViewModel.Status.Success).forecast == forecast)
+    }
+
+    @Test
+    fun `Location Permission denied`(){
+        assert(weatherViewModel.currentStatus.value is WeatherViewModel.Status.None)
+
+        weatherViewModel.onLocationPermissionDenied()
+
+        assert(weatherViewModel.currentStatus.value is WeatherViewModel.Status.Error)
+        assert((weatherViewModel.currentStatus.value as WeatherViewModel.Status.Error).errorType==WeatherViewModel.ErrorType.LOCATION_PERMISSION_DENIED)
     }
 
 }
